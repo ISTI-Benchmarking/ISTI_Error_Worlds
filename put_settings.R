5e1279ae9f212a3c440f31f00445c6cde3b4043f
Victor Venema <victor.venema@uni-bonn.de>
2015-08-10 16:07:02 +0100

    Cleaned up the new function to read the settings.

--- put_settings.R
+++ put_settings.R
@@ -1,22 +1,27 @@
-put_settings <- function(settingsRead, oldSettings) {
+put_settings <- function(settingsRead, settings) {
   # This function accepts the two column character list settingsRead and puts these values
   # in the structure settings.
   # The first column of settingsRead is the variableName, the second the variableValue.
   # setting looks like: settings$variableName = variableValue
   
-  if(missing(oldSettings)) {
-    # Initialise settings list
-    noSettings = length(settingsRead$settingName)
-    settings = vector("list", length=noSettings)
+  # Initialise
+  noSettings <- length(settingsRead$settingName)
+  
+  # Put settingsRed in the settings structure
+  if(missing(settings)) {
+    # Initialise settings list (with the default settings)
+    settings <- vector("list", length=noSettings)
     for(iSet in 1:noSettings) {
       settings[iSet] <- settingsRead$settingValue[iSet]
       names(settings)[iSet] <- as.character(settingsRead$settingName[iSet])
     }
   } else {
-    print("oldSettings present")
-    
+    # (Default) settings present. Update the settings for a specific world with those that are new.
+    for(iSet in 1:noSettings) {
+      index <- which( names(settings) == as.character(settingsRead$settingName[iSet]) )
+      settings[[index]] <- settingsRead$settingValue[iSet]
+    }
   }
-   
-   
+  
   return(settings)
  }
--- read_settings.R
+++ read_settings.R
@@ -7,46 +7,49 @@ read_settings <- function(currentDirs) {
 
   # TODO: The part for network specific settings is not implemented yet.
   
+  # Initialise
+  source("put_settings.R")
+  
   settingsDir <- currentDirs$cleanDir
   defaultSettingsDir <- dirname(settingsDir)
-#   str(settingsDir)
-#   str(defaultSettingsDir)
 
   # Read default settings file.
-  settingsDirFileName = paste(defaultSettingsDir, "default_settings.txt", sep="/")
+  settingsDirFileName <- paste(defaultSettingsDir, "default_settings.txt", sep="/")
   if(file.exists(settingsDirFileName) == FALSE) {
     # If no default settings file exists yet, make one with default values
-#     settingName  = c("globalBreakFrequency", "networkBreakFrequencyStd", "stationBreakFrequencyStd")
-#     settingValue = c(5,                       1,                          1)
-#     df = data.frame(settingName, settingValue)
-#     write(df, file = settingsDirFileName)
-#     settingName  = c("globalBreakSize", "networkBreakSizeStd", "stationBreakFrequencyStd")
-#     settingValue = c(0.6,               0.2,                   0)
-#     df = data.frame(settingName, settingValue)
-#     write(df, file = settingsDirFileName, append = TRUE)    
-    
-    settingsStr = c("# This file contains the default settings for all error worlds.",
-                 "# These settings can be overrided by more detailed settings in the subdirectories.",
-                 "# Comments can be added by starting the line with the pound sign, like this header.",
-                 "# You can edit this file to update the settings. Any white space between name and value works.",
-                 "# If you delete it, it will be replaced by the defaults mentioned in the function read_settings().",
-                 "globalBreakFrequency 5",
-                 "networkBreakFrequencyStd 1",
-                 "stationBreakFrequencyStd 1", 
-                 "globalBreakSize 0.6",
-                 "networkBreakSizeStd 0.2",
-                 "stationBreakFrequencyStd 0")
-    write(settingsStr, file = settingsDirFileName)
+    if(file.exists("default_default_settings.txt") == TRUE) {
+      file.copy(from="default_default_settings.txt", to=settingsDirFileName)
+    } else {
+      stop('No settingfile found. Processing aborted in read_settings.R')
+    }
   }
-  settingsRead = read.table(settingsDirFileName, col.names=c("settingName", "settingValue")) #  colClasses=c("character"))
-  settings = put_settings(settingsRead)
+  settingsRead <- read.table(settingsDirFileName, col.names=c("settingName", "settingValue")) 
+  settings <- put_settings(settingsRead) # Put values read in a structure with the names read.
 
   # Read world settings file.
-
+  settingsDirFileName <- paste(settingsDir, "world_settings.txt", sep="/")
+  if(file.exists(settingsDirFileName) ) {
+    settingsRead <- read.table(settingsDirFileName, col.names=c("settingName", "settingValue")) 
+    settings <- put_settings(settingsRead, settings) # Put values read in a structure with the names read, overwriting defaults if double.
+  }
 
   # Read network settings file.
+  # TODO
 
-
-  settings = 0
   return(settings)
 }
+
+
+## Wastebin
+#     settingsStr = c("# This file contains the default settings for all error worlds.",
+#                  "# These settings can be overrided by more detailed settings in the subdirectories.",
+#                  "# Comments can be added by starting the line with the pound sign, like this header.",
+#                  "# You can edit this file to update the settings. Any white space between name and value works.",
+#                  "# If you delete it, it will be replaced by the defaults mentioned in the function read_settings().",
+#                  "globalBreakFrequency 5",
+#                  "networkBreakFrequencyStd 1",
+#                  "stationBreakFrequencyStd 1", 
+#                  "globalBreakSize 0.6",
+#                  "networkBreakSizeStd 0.2",
+#                  "stationBreakFrequencyStd 0")
+#     write(settingsStr, file = settingsDirFileName)
